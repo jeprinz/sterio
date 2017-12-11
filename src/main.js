4ffcebf985cb953c94662359df80d55e8d7dba77
jacob5555555555 <jacobeliasprinz@gmail.com>
2017-12-10 19:58:18 -0500

    Setup graphics generally

--- src/main.js
+++ src/main.js
@@ -3,7 +3,19 @@
 // This is the main file that will load in the browser
 
 import only from "onlyjs"
+import {setupCanvas} from "./graphics"
 
-console.log("sdlkfjsdf")
+//First set up page
 
-only.setHtml({p: "Hello"})
+const canvas = only.html({canvas: "", width: 500, height: 500,
+  css: {//Border so we can see where canvas is
+    borderStyle: "solid"
+}});
+
+
+only.setHtml({div: [
+  {p: "Welcome to SterIO, a very intentional pun"},
+  canvas
+]});
+
+setupCanvas(canvas);//This function passes off control to the graphics code
