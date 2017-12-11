7137818efe4099f0dc4be017d98b1f27d2d51f3b
jacob5555555555 <jacobeliasprinz@gmail.com>
2017-12-10 20:20:37 -0500

    Plugged in draw function

--- src/graphics.js
+++ src/graphics.js
@@ -1,9 +1,11 @@
 // @flow
 
+import {drawStereogram} from "./genStereogram"
+
 //This file sets up canvas and ties stereogram code to frontend code
 
-const WIDTH = 500;
-const HEIGHT = 500;
+const WIDTH = 1024;
+const HEIGHT = 768;
 
 //Takes an HTML canvas and sets it up with steriogram code
 export function setupCanvas(canvas: HTMLCanvasElement){
@@ -16,12 +18,18 @@ export function setupCanvas(canvas: HTMLCanvasElement){
     alert("Failed to initialize canvas.")
   } else {
     (ctx : CanvasRenderingContext2D)//Tell flow what type the ctx object is
-    ctx.fillStyle = 'rgb(23, 100, 30)'
-    ctx.fillRect(1, 1, 10, 10)
 
     const imageData = new ImageData(WIDTH, HEIGHT)
     const drawFunction = makeDrawPixelFunction(imageData)
-    ctx.putImageData(imageData, 0, 0, 0, 0)
+
+    console.log(imageData.data.length)
+
+    //////////////////////////////////////////////////////
+    drawStereogram(drawFunction, WIDTH, HEIGHT);
+    //////////////////////////////////////////////////////
+
+
+    ctx.putImageData(imageData, 0, 0)
   }
 
 }
@@ -29,9 +37,10 @@ export function setupCanvas(canvas: HTMLCanvasElement){
 function makeDrawPixelFunction(imageData: ImageData){
   return function(x: number, y: number, value: number){
     const pixel = x + y*WIDTH;
-    const arrayElement = pixel*3
+    const arrayElement = pixel*4
     imageData.data[arrayElement] = value
     imageData.data[arrayElement + 1] = value
     imageData.data[arrayElement + 2] = value
+    imageData.data[arrayElement + 3] = 255
   }
 }
